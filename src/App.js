d1f31df1437b8743f865ae71746b88baf060cb9a
Giovanni Bianciardi <giovanni.bianciardi@realitycs.it>
2023-12-05 17:30:57 +0100

    setup flask_react finito

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // new line start
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  function getData() {
+    setLoading(true);
+    axios({
+      method: "GET",
+      url: "/profile",
+    })
+      .then((response) => {
+        setLoading(false);
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+  //end of new line
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <div>
+            <p>To get your profile details: </p>
+            <button onClick={getData}>Click me</button>
+            {profileData && (
+              <div>
+                <p>Profile name: {profileData.profile_name}</p>
+                <p>About me: {profileData.about_me}</p>
+              </div>
+            )}
+          </div>
+        )}
       </header>
     </div>
   );
