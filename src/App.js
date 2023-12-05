2c100b6cd68e79797e390229f5f197c98f8ec34f
Giovanni Bianciardi <giovanni.bianciardi@realitycs.it>
2023-12-05 17:53:12 +0100

    setup router

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
-import { useState } from "react";
-import axios from "axios";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./components/Layout";
+import NoPage from "./pages/NoPage";
 
-function App() {
-  // new line start
-  const [profileData, setProfileData] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  function getData() {
-    setLoading(true);
-    axios({
-      method: "GET",
-      url: "/profile",
-    })
-      .then((response) => {
-        setLoading(false);
-        const res = response.data;
-        setProfileData({
-          profile_name: res.name,
-          about_me: res.about,
-        });
-      })
-      .catch((error) => {
-        setLoading(false);
-        if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  }
-  //end of new line
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {loading ? (
-          <p>Loading</p>
-        ) : (
-          <div>
-            <p>To get your profile details: </p>
-            <button onClick={getData}>Click me</button>
-            {profileData && (
-              <div>
-                <p>Profile name: {profileData.profile_name}</p>
-                <p>About me: {profileData.about_me}</p>
-              </div>
-            )}
-          </div>
-        )}
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { useState } from "react";
+import axios from "axios";
+
+function HomePage() {
+  // new line start
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  function getData() {
+    setLoading(true);
+    axios({
+      method: "GET",
+      url: "/profile",
+    })
+      .then((response) => {
+        setLoading(false);
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+  //end of new line
+
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <div>
+            <p>To get your profile details: </p>
+            <button onClick={getData}>Click me</button>
+            {profileData && (
+              <div>
+                <p>Profile name: {profileData.profile_name}</p>
+                <p>About me: {profileData.about_me}</p>
+              </div>
+            )}
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
