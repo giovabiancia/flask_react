6f5946936675528e76ee93815397305cdbec9d9b
Giovanni Bianciardi <giovanni.bianciardi@realitycs.it>
2023-12-05 18:02:35 +0100

    wip

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { useState } from "react";
 import axios from "axios";
-import { Col, Container, Row } from "react-bootstrap";
 
 function HomePage() {
   // new line start
